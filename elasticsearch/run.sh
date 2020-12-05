65cd42d0bcdf42b916e38b019204ede414986939
Mark Ditsworth <markditsworth@protonmail.com>
2020-12-05 17:34:17 -0600

    configure after launching container

--- elasticsearch/run.sh
+++ elasticsearch/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 docker network create dnetwork --driver bridge
 docker run -d -p 9200:9200 -p 9300:9300 --network dnetwork -v $(pwd)/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml --name elasticsearch elasticsearch:7.9.3
+
+wait 5
+
+./configure.sh
